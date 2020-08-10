237c64a1e9e8b7910427d9e9d6f52308401cdb18
Lind.Shabani <lind.shabani@solaborate.com>
2020-08-10 10:42:46 +0200

    adding auth

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-const express = require("express")
 require("./db/mongoose")
-const Task = require("./models/task")
+const express = require("express")
 const userRouter = require("./routers/user")
 const taskRouter = require("./routers/task")
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
+
+// app.use((req, res, next) => {
+//   res.status(503).send("website is in maintnace")
+// })
 
 app.use(express.json())
 app.use(userRouter)
@@ -14,3 +17,18 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log("Server is up and running on port", port)
 })
+
+// const Task = require("./models/task")
+// const User = require("./models/user")
+
+// const main = async () => {
+//   // const task = await Task.findById("5f2d6e76052bd23c7206b807")
+//   // await task.populate("owner").execPopulate()
+//   // console.log(task.owner) 
+
+//   const user = await User.findById("5f2d6e4f052bd23c7206b805")
+//   await user.populate("tasks").execPopulate()
+//   console.log(user.tasks)
+// }
+
+// main()
